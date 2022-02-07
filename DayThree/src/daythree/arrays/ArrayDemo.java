cbf37664ac74fdd8a9beceb6fe6c5fe62be1b904
Acronikhil <20.nikhildubey@gmail.com>
2022-02-07 09:54:04 +0530

    7 Feb Monday

---
+++ DayThree/src/daythree/arrays/ArrayDemo.java
@@ -0,0 +1,12 @@
+package daythree.arrays;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		String arr [] = {"hello","Nikhil", "Dubey", "How", "Are", "You"};
+		
+		for(String x :arr) {
+			System.out.println(x);
+		}
+	}
+}
